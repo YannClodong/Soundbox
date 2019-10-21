4a79db06deff76a9d91de178d4f956f7019c4787
Yann Clodong <yann.clodongmail.com>
2019-10-21 16:47:21 +0200

    change player

--- readSound.py
+++ readSound.py
@@ -1,10 +1,7 @@
-import pygame
+import os
 
 def read(file):
-    pygame.mixer.init()
-    pygame.mixer.music.load(file)
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy() == True:
-        continue
+    os.system("omxplayer " + file)
+
 
 
